a8fb63b432b00b8df05bb3cf83d142a4429b29be
tmhuyett <toddhuyett@gmail.com>
2021-04-07 15:21:46 -0400

    'd'

--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,83 @@
-import Layout from '@components/Layout'
-import PostList from '@components/PostList'
-import Link from 'next/link'
-import { SocialIcon } from 'react-social-icons';
+import Layout from "@components/Layout";
+import PostList from "@components/PostList";
+import Link from "next/link";
+import { SocialIcon } from "react-social-icons";
+import Head from "next/head";
 
-import getPosts from '@utils/getPosts'
+import getPosts from "@utils/getPosts";
 
 const Index = ({ posts, title, description, ...props }) => {
   return (
     <>
+      <Head>
+        <link
+          href="//db.onlinewebfonts.com/c/37b29158c68de167677fa4a3087d7188?family=Tiempos+Text+Regular"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
       <Layout pageTitle={title} description={description}>
-
         <h1 className="title">Todd Huyett</h1>
         <h2>Web Developer</h2>
 
-
-          
-
-        <h3 className="description">
-          Recent projects:
-        </h3>
+        <h3 className="description">Recent projects:</h3>
         <main>
           <PostList posts={posts} />
         </main>
 
         <h3>About:</h3>
-        <p>Before getting into JavaScript, I taught English in various countries. I enjoy coding, Byzantine history, perfumery, squirrels, and linguistics. As a developer, I enjoy learning to use new frameworks and tools, which is how I spend most of my free time. </p>
-   
+        <p>
+          Before getting into JavaScript, I taught English in various countries.
+          I enjoy coding, Byzantine history, perfumery, squirrels, and
+          linguistics. As a developer, I enjoy learning to use new frameworks
+          and tools, which is how I spend most of my free time.{" "}
+        </p>
       </Layout>
       <style jsx>{`
         .title {
-       
-          font-size: calc(30px + .5vw);
+          font-size: calc(30px + 0.5vw);
           padding-left: 20px;
           color: black;
-       
-         
         }
         h1 {
-      
           padding-left: 20px;
           style: none;
-         
         }
         h2 {
           padding-left: 22px;
-          font-size: calc(18px + .5vw);
-     
-          
+          font-size: calc(18px + 0.5vw);
+
           style: bold;
-        
+
           padding-top: 0;
           margin-top: -20px;
           color: black;
-    
-      
-          
         }
         h3 {
           padding-left: 20px;
           padding-bottom: 0;
           margin-bottom: 0;
-          
         }
-
-
 
         p {
           padding-left: 20px;
           font-size: 18px;
           padding-bottom: 0;
           margin-bottom: 0;
-
-      
-          
         }
-
-
-
- 
-        
       `}</style>
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
 export async function getStaticProps() {
-  const configData = await import(`../siteconfig.json`)
+  const configData = await import(`../siteconfig.json`);
 
   const posts = ((context) => {
-    return getPosts(context)
-  })(require.context('../posts', true, /\.md$/))
+    return getPosts(context);
+  })(require.context("../posts", true, /\.md$/));
 
   return {
     props: {
@@ -99,5 +85,5 @@ export async function getStaticProps() {
       title: configData.default.title,
       description: configData.default.description,
     },
-  }
+  };
 }
--- posts/edibles.md
+++ posts/edibles.md
@@ -1,5 +1,5 @@
 ---
-title: "Gatsby + Shopify Nature Shop"
+title: "Gatsby + Shopify Icon Shop"
 author: "Todd"
 date: "2020-04-29"
 ---
@@ -7,4 +7,5 @@ date: "2020-04-29"
 The purpose of this had nothing to do with artistic beauty. I merely wanted to learn to configure Gatsby and Shopify.
 
 #### [Live Demo](https://optimistic-spence-02adf6.netlify.app/)
+
 #### [Repo](https://github.com/prokopious/gatShop/)
